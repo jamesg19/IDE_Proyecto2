fa3d71f31108e431c58e7961afa8957924f54f14
james <james@DESKTOP-BTERTCJ>
2020-10-12 19:09:39 -0400

    nodo de la pila

---
+++ IDEjames/Sintactico/Nodo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDEjames.Sintactico
+{
+    class Nodo
+    {
+        public char informacion;
+        public Nodo siguiente;
+    }
+}

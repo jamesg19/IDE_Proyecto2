b509d3631723e3d12838465206d97bf30c34f22f
james <james@DESKTOP-BTERTCJ>
2020-10-22 19:56:54 -0400

    solucionando error de tokens con la tabla

---
+++ IDEjames/Analizador/Tabla.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDEjames.Analizador
+{
+    class Tabla
+    {
+        int numeroFilas = 40;
+        int numeroColumnas = 20;
+        int[,] tabla;
+
+        public TablaTransiciones()
+        {
+            asignarValores();
+        }
+    }
+}
